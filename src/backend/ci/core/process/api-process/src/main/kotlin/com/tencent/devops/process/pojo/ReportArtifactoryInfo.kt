f424c7429040af541b1c40a9d82275c2127b337b
yjieliang <yundantengteng@163.com>
2022-07-14 18:48:00 +0800

    feat: CI制品增加更多类型 #6302

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/pojo/ReportArtifactoryInfo.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/pojo/ReportArtifactoryInfo.kt
@@ -6,16 +6,18 @@ import io.swagger.annotations.ApiModelProperty
 data class ReportArtifactoryInfo(
     @ApiModelProperty("构件名称", required = true)
     val name: String,
-    @ApiModelProperty("构件tag", required = true)
-    val tag: String?,
+    @ApiModelProperty("构件tag", required = false)
+    val tag: String? = null,
     @ApiModelProperty("构件仓库路径", required = true)
     val path: String,
+    @ApiModelProperty("文件全路径", required = true)
+    val fullPath: String,
     @ApiModelProperty("构件大小", required = true)
     val size: Long,
     @ApiModelProperty("仓库类型", required = true)
     val type: ArtifactoryType,
-    @ApiModelProperty("是否文件夹", required = true)
-    val folder: Boolean,
+    @ApiModelProperty("是否文件夹", required = false)
+    val folder: Boolean? = false,
     @ApiModelProperty("项目ID", required = true)
     val projectId: String,
     @ApiModelProperty("构建ID", required = true)
