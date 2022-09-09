860264c30c98a7fd71e23de14f2590acab3f878a
yaoxuwan <yaoxuwan@tencent.com>
2022-09-09 10:05:25 +0800

    feat: CI制品增加更多类型 #6302 合并bkrepoClient

--- src/backend/ci/ext/tencent/common/common-archive-tencent/src/main/kotlin/com/tencent/devops/common/archive/BkRepoAutoConfiguration.kt
+++ src/backend/ci/ext/tencent/common/common-archive-tencent/src/main/kotlin/com/tencent/devops/common/archive/BkRepoAutoConfiguration.kt
@@ -27,17 +27,11 @@
 
 package com.tencent.devops.common.archive
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.tencent.devops.common.archive.client.BkRepoClient
 import com.tencent.devops.common.archive.client.DirectBkRepoClient
-import com.tencent.devops.common.archive.config.BkRepoConfig
-import com.tencent.devops.common.service.config.CommonConfig
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.AutoConfigureOrder
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.core.Ordered
 
 @Configuration
@@ -45,16 +39,16 @@ import org.springframework.core.Ordered
 @AutoConfigureOrder(Ordered.LOWEST_PRECEDENCE)
 class BkRepoAutoConfiguration {
 
-    @Bean
-    fun bkRepoConfig() = BkRepoConfig()
-
-    @Bean
-    @Primary
-    fun bkRepoClient(
-        @Autowired objectMapper: ObjectMapper,
-        @Autowired commonConfig: CommonConfig,
-        @Autowired bkRepoConfig: BkRepoConfig
-    ) = BkRepoClient(objectMapper, commonConfig, bkRepoConfig)
+//    @Bean
+//    fun bkRepoConfig() = BkRepoConfig()
+//
+//    @Bean
+//    @Primary
+//    fun bkRepoClient(
+//        @Autowired objectMapper: ObjectMapper,
+//        @Autowired commonConfig: CommonConfig,
+//        @Autowired bkRepoConfig: BkRepoConfig
+//    ) = BkRepoClient(objectMapper, commonConfig, bkRepoConfig)
 
     @Bean
     fun directBkRepoClient() = DirectBkRepoClient()
