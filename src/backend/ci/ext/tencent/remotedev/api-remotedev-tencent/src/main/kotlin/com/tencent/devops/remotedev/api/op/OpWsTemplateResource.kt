e6e5f515189eff3dbbb1b04d5f0b7157fab00ff1
sawyersong <sawyersong@tencent.com>
2022-11-25 21:35:04 +0800

    --story=879960659 创建和启动工作空间（增删查）

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/op/OpWsTemplateResource.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/op/OpWsTemplateResource.kt
@@ -64,7 +64,7 @@ interface OpWsTemplateResource {
 
     @ApiOperation("更新工作空间模板")
     @PUT
-    @Path("/update")
+    @Path("/update/template/{wsTemplateId}")
     fun updateWorkspaceTemplate(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
