39aa463e7a2985ef87bd5490598bd0ab5ba0c350
yaoxuwan <yaoxuwan@tencent.com>
2022-12-12 16:23:50 +0800

    feat: 构建详情中制品查询提供新接口 #8097 单次构建查询产出物

--- src/backend/ci/core/artifactory/api-artifactory/src/main/kotlin/com/tencent/devops/artifactory/pojo/PipelineOutputSearchOption.kt
+++ src/backend/ci/core/artifactory/api-artifactory/src/main/kotlin/com/tencent/devops/artifactory/pojo/PipelineOutputSearchOption.kt
@@ -3,5 +3,5 @@ package com.tencent.devops.artifactory.pojo
 import com.tencent.devops.artifactory.pojo.enums.PipelineOutputType
 
 data class PipelineOutputSearchOption(
-    val pipelineOutputType: PipelineOutputType? = null
+    val pipelineOutputType: PipelineOutputType?
 )
